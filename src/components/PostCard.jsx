9780363974db9f7db572eb97606e3205ef98bb1d
Jake Overall <joverall22@gmail.com>
2023-02-08 12:19:46 -0700

    likes posts

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,21 +1,58 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { AppState } from '../AppState.js';
+import { postsService } from '../services/PostsService.js';
+import Pop from '../utils/Pop.js';
 import './PostCard.scss';
 
 // eslint-disable-next-line react/prop-types
 export default function PostCard({ post }) {
 
-  const hasImage = (post.imgUrl ? <img className="post-image rounded-top" src={post.imgUrl} alt="" /> : null)
+  async function likePost() {
+    try {
+      await postsService.likePost(post)
+    }
+    catch (error) {
+      Pop.error(error);
+    }
+  }
+
+
+  const hasImage = (post.imgUrl ? <img className="post-image" src={post.imgUrl} alt="" /> : null)
+
+  const hasLiked = post.likeIds.includes(AppState.account?.id)
 
 
   return (
 
     <div className="PostCard card p-0">
+      <div className="d-flex px-2 align-items-center justify-content-between">
+        <div className='p-2 d-flex gap-3 align-items-center'>
+          <img src={post.creator.picture} alt="" height={42} width={42} className="rounded-circle" />
+          <p className="mb-0">{post.creator.name}</p>
+        </div>
+        <div>
+
+          <small>{new Date(post.createdAt).toLocaleString('en-US', { dateStyle: 'medium', timeStyle: 'short' })}</small>
+
+        </div>
+      </div>
       {hasImage}
       <div className="card-body">
         <p>
           {post.body}
         </p>
+        <div className='text-end'>
+          <span className='selectable rounded'>
+            {
+              hasLiked
+              ? <span onClick={likePost}>💖</span>
+              : <span onClick={likePost}>💟</span>
+            }
+          </span>
+          <span>{post.likeIds.length}</span>
+
+        </div>
       </div>
     </div>
   )
@@ -23,40 +60,3 @@ export default function PostCard({ post }) {
 }
 
 
-/**
- * 
- * {
-  "_id": "63d05197c19e5ea56d477b85",
-  "body": "Good luck juniors! LT Fall 2022 Rules",
-  "imgUrl": "",
-  "creatorId": "63925b1dabc8982f6134300f",
-  "likeIds": [],
-  "createdAt": "2023-01-24T21:45:59.896Z",
-  "updatedAt": "2023-01-24T21:45:59.896Z",
-  "__v": 0,
-  "creator": {
-    "_id": "63925b1dabc8982f6134300f",
-    "subs": [
-      "auth0|63925b1d4934078fe0069b75"
-    ],
-    "email": "derek@derek.dev",
-    "name": "Derek Hearst!",
-    "picture": "https://avatars.githubusercontent.com/u/12779264?v=4",
-    "bio": "Wassup peeps boys :)\n",
-    "coverImg": "https://images.unsplash.com/photo-1670779757037-6de9257f8e0b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
-    "github": "https://github.com/derekhearst/",
-    "linkedin": "https://www.linkedin.com/in/derekhearst/",
-    "resume": "https://derekhearst.com",
-    "class": "Late Fall 2027",
-    "graduated": true,
-    "createdAt": "2022-12-08T21:57:48.617Z",
-    "updatedAt": "2022-12-12T17:28:32.433Z",
-    "__v": 0,
-    "id": "63925b1dabc8982f6134300f"
-  },
-  "likes": [],
-  "id": "63d05197c19e5ea56d477b85"
-}
- * 
- * 
- */
